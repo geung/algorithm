ca934233e6d520697499cd664f4448883bbafb97
geung <pabc98@naver.com>
2022-01-24 00:27:27 +0900

    0124

--- IdeaProjects/javastudy/src/backjoon/세수정렬.java
+++ IdeaProjects/javastudy/src/backjoon/세수정렬.java
@@ -9,16 +9,19 @@ public class 세수정렬 {
         for(int i=0;i<3;i++){
             array[i] = sc.nextInt();
         }
-        int min = 9999;
-        int index = 0;
-        for(int i=0;i<array.length;i++){
-          for(int j=i;j<array.length;j++){
+
+        int index=0;
+        int tmp;
+        int min, i,j;
+        for(i=0;i<3;i++){
+            min =10000001;
+          for(j=i;j<3;j++){
               if(min > array[j]){
                  min = array[j];
                  index = j;
               }
           }
-          int tmp = array[i];
+           tmp = array[i];
           array[i] = array[index];
           array[index] = tmp;
         }
